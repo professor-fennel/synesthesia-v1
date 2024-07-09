7905412f146be9e9f1159c76e963d262be40460a
professor-fennel <62268736+professor-fennel@users.noreply.github.com>
2024-07-09 14:12:31 -0400

    c pentatonic mode and continuous mode

--- main.py
+++ main.py
@@ -1,8 +1,105 @@
-light_level = 0
+def on_button_pressed_a():
+    global alive
+    if alive == 1:
+        alive = 0
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+    else:
+        alive = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global mode
+    if mode == 1:
+        mode = 0
+        basic.show_leds("""
+            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # # .
+            """)
+    else:
+        mode = 1
+        basic.show_leds("""
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+hz = 0
+alive = 0
+mode = 0
+mode = 0
+alive = 1
+c_major_scale = [41.2,
+    49,
+    55,
+    65.4,
+    73.4,
+    82.4,
+    98,
+    110,
+    130.8,
+    146.8,
+    164.8,
+    196,
+    220,
+    261.6,
+    293.7,
+    329.6,
+    392,
+    440,
+    523.3,
+    587.3,
+    659.3,
+    783.99,
+    880,
+    1046.5,
+    1174.7,
+    1318.5,
+    1568,
+    1760]
+basic.show_leds("""
+    . # # # .
+    # . . . .
+    # . . . .
+    # . . . .
+    . # # # .
+    """)
 
 def on_forever():
-    global light_level
-    light_level = pins.digital_read_pin(DigitalPin.P0)
-    music.play(music.tone_playable(light_level, music.beat(BeatFraction.WHOLE)),
-        music.PlaybackMode.UNTIL_DONE)
+    global hz
+    if mode == 0:
+        hz = c_major_scale[Math.floor(Math.map(pins.analog_read_pin(AnalogPin.P0),
+                0,
+                900,
+                0,
+                len(c_major_scale) - 1))]
+    else:
+        hz = Math.map(pins.analog_read_pin(AnalogPin.P0),
+            0,
+            900,
+            c_major_scale[0],
+            c_major_scale[len(c_major_scale) - 1])
+    makerbit.show_string_on_lcd1602("P0", makerbit.position1602(LcdPosition1602.POS1), 2)
+    makerbit.show_string_on_lcd1602("" + str(pins.analog_read_pin(AnalogPin.P0)),
+        makerbit.position1602(LcdPosition1602.POS4),
+        12)
+    makerbit.show_string_on_lcd1602("HZ", makerbit.position1602(LcdPosition1602.POS17), 2)
+    makerbit.show_string_on_lcd1602("" + str(hz),
+        makerbit.position1602(LcdPosition1602.POS20),
+        12)
+    if alive == 1:
+        music.ring_tone(hz)
+    else:
+        music.stop_all_sounds()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,93 @@
 input.onButtonPressed(Button.A, function () {
     if (alive == 1) {
         alive = 0
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     } else {
         alive = 1
     }
 })
-let light_level = 0
+input.onButtonPressed(Button.B, function () {
+    if (mode == 1) {
+        mode = 0
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # # .
+            `)
+    } else {
+        mode = 1
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    }
+})
+let hz = 0
 let alive = 0
+let mode = 0
+mode = 0
 alive = 1
+let c_maj_pent_scale = [
+41.2,
+49,
+55,
+65.4,
+73.4,
+82.4,
+98,
+110,
+130.8,
+146.8,
+164.8,
+196,
+220,
+261.6,
+293.7,
+329.6,
+392,
+440,
+523.3,
+587.3,
+659.3,
+783.99,
+880,
+1046.5,
+1174.7,
+1318.5,
+1568,
+1760
+]
+basic.showLeds(`
+    . # # # .
+    # . . . .
+    # . . . .
+    # . . . .
+    . # # # .
+    `)
 basic.forever(function () {
-    light_level = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 900, 100, 700)
+    if (mode == 0) {
+        hz = c_maj_pent_scale[Math.floor(Math.map(pins.analogReadPin(AnalogPin.P0), 0, 900, 0, c_maj_pent_scale.length - 1))]
+    } else {
+        hz = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 900, c_maj_pent_scale[0], c_maj_pent_scale[c_maj_pent_scale.length - 1])
+    }
     makerbit.showStringOnLcd1602("P0", makerbit.position1602(LcdPosition1602.Pos1), 2)
-    makerbit.showStringOnLcd1602("" + (pins.analogReadPin(AnalogPin.P0)), makerbit.position1602(LcdPosition1602.Pos4), 12)
-    makerbit.showStringOnLcd1602("LL", makerbit.position1602(LcdPosition1602.Pos17), 2)
-    makerbit.showStringOnLcd1602("" + (light_level), makerbit.position1602(LcdPosition1602.Pos20), 12)
+    makerbit.showStringOnLcd1602("" + pins.analogReadPin(AnalogPin.P0), makerbit.position1602(LcdPosition1602.Pos4), 12)
+    makerbit.showStringOnLcd1602("HZ", makerbit.position1602(LcdPosition1602.Pos17), 2)
+    makerbit.showStringOnLcd1602("" + hz, makerbit.position1602(LcdPosition1602.Pos20), 12)
     if (alive == 1) {
-        music.play(music.tonePlayable(light_level, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        music.ringTone(hz)
+    } else {
+        music.stopAllSounds()
     }
 })
